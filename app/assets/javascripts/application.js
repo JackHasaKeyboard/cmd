c44b43f87442545e05db259972df3469661d3a81
jackhasakeyboard <jackhasakeyboard@gmail.com>
2016-09-16 12:21:04 -0600

    Update css and js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,31 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(document).on('turbolinks:load', function(){
+	$("tr").each(function(){
+		$(this).find("td:not(:empty):not(:last)").addClass("key");
+
+		$(this).find(".key:not(:last)").first().css({
+			"border-radius": "4px 0 0 4px"
+		});
+
+		$(this).find(".key:not(:first)").last().css({
+			"border-radius": "0 4px 4px 0"
+		});
+	});
+
+	$(".key").css({
+		"background": "#e9e9e9",
+		"text-align": "center",
+		"color": "#bcbcbc",
+		"border": "1px solid #bcbcbc",
+		"border-bottom": "2px solid #bcbcbc",
+		"-webkit-touch-callout": "none", 
+		"-webkit-user-select": "none",	 
+		"-khtml-user-select": "none",		 
+		"-moz-user-select": "none",			 
+		"-ms-user-select": "none",			 
+		"user-select": "none"				
+	});
+});
