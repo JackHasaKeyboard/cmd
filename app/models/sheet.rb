8a6f9b4db8245600ed869a1dd6a6f462799f5480
jackhasakeyboard <jackhasakeyboard@gmail.com>
2016-10-03 17:22:14 +0000

    Validate sheet name's uniqueness, can no longer create duplicate slugs that resolve to a random string being appended on

--- app/models/sheet.rb
+++ app/models/sheet.rb
@@ -1,6 +1,7 @@
 class Sheet < ApplicationRecord
 	has_attached_file :md
 	validates_attachment_file_name :md, matches: [/md\Z/]
+	validates_uniqueness_of :name
 
 	extend FriendlyId
 	friendly_id :name, use: :slugged
